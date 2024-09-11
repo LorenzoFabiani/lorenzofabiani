d1f8518c5621723f91a68955c5b5ebe610f788f1
LorenzoFabiani <lorenzo.fabiani1@gmail.com>
2024-09-11 18:11:01 +0200

    Update data.tsx
    Signed-off-by: LorenzoFabiani <lorenzo.fabiani1@gmail.com>

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -327,7 +327,8 @@ export const testimonial: TestimonialSection = {
   testimonials: [
     {
       name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+      text: 'Collaborare con me significa lavorare con un professionista appassionato di dati e innovazione, sempre pronto a trovare soluzioni creative e a trasformare le sfide in opportunità. 
+        Il mio approccio è basato su trasparenza, rispetto delle scadenze e attenzione ai dettagli. Ecco cosa dicono di me colleghi e clienti soddisfatti.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
     },
   ],
