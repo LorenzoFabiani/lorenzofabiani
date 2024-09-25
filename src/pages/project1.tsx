b7f0e1f50d1313bb811d1d37a87b10e4f8435c98
LorenzoFabiani <lorenzo.fabiani1@gmail.com>
2024-09-25 23:49:14 +0200

    Create project1.tsx
    Signed-off-by: LorenzoFabiani <lorenzo.fabiani1@gmail.com>

---
+++ src/pages/project1.tsx
@@ -0,0 +1,14 @@
+// src/pages/Project1.tsx
+import React from 'react';
+
+const Project1 = () => {
+  return (
+    <div>
+      <h1>Project Title 1</h1>
+      <p>This is a detailed description of Project 1. In this project, I analyzed...</p>
+      <p>Here you can explain the methodology, tools used, and results obtained.</p>
+    </div>
+  );
+};
+
+export default Project1;
