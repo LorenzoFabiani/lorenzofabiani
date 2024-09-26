efc892f64468cd08bccbbb6d069ce174b41ddbdf
LorenzoFabiani <lorenzo.fabiani1@gmail.com>
2024-09-26 18:20:31 +0200

    Create ImpunityIndex.tsx
    Signed-off-by: LorenzoFabiani <lorenzo.fabiani1@gmail.com>

---
+++ src/pages/ImpunityIndex.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ImpunityIndex = () => {
+  return (
+    <div>
+      <h1>The Role of Impunity: Proposal for a Composite Index</h1>
+      <p>Abstract: [Inserisci l'abstract del lavoro]</p>
+      <p>Additional details or information</p>
+    </div>
+  );
+};
+
+export default ImpunityIndex;
