90bef282bbc9664bd57899e2a82b5eff31892b95
LorenzoFabiani <lorenzo.fabiani1@gmail.com>
2024-09-27 01:25:57 +0200

    Update ImpunityIndex.tsx
    Signed-off-by: LorenzoFabiani <lorenzo.fabiani1@gmail.com>

--- src/pages/ImpunityIndex.tsx
+++ src/pages/ImpunityIndex.tsx
@@ -5,8 +5,8 @@ import Page from '../components/Layout/Page';
 
 const ImpunityIndexPageMeta = {
   title: 'The Role of Impunity: Proposal for a Composite Index',
-  description: 'Authors: Mario Eboli and Lorenzo Fabiani
-Presented at 14th Annual Conference Spanish Association of Law and Economics – AEDE, Barcelona, June 27-28, 2024.',
+  description: `Authors: Mario Eboli and Lorenzo Fabiani
+Presented at the 14th Annual Conference Spanish Association of Law and Economics – AEDE, Barcelona, June 27-28, 2024.`,
 };
 
 // eslint-disable-next-line react-memo/require-memo
