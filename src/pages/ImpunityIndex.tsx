2d76a6210c9f1a01c1a28edac519c2c323fd3ecc
LorenzoFabiani <lorenzo.fabiani1@gmail.com>
2024-09-26 18:59:01 +0200

    Update ImpunityIndex.tsx
    Signed-off-by: LorenzoFabiani <lorenzo.fabiani1@gmail.com>

--- src/pages/ImpunityIndex.tsx
+++ src/pages/ImpunityIndex.tsx
@@ -19,21 +19,29 @@ const ImpunityIndex: FC = memo(() => {
   return (
     <Page description={description} title={title}>
       <Header />
-
       {/* Sezione personalizzata per l'abstract */}
-      <section style={{padding: '20px'}}>
-        <h1>{title}</h1>
-        <p>{description}</p>
-        <p>
-          <strong>Abstract:</strong> This paper introduces a composite index for impunity,
-          which aims to measure regional disparities in Italy from 2008 to 2021. The index highlights
-          the significant socio-economic impacts of impunity, especially on foreign direct investments
-          and crime reporting. Through an econometric analysis, the findings emphasize the need for
-          stronger legal frameworks and targeted policies to reduce regional disparities and promote
-          socio-economic stability.
-        </p>
+      <section style={{
+        display: 'flex', 
+        flexDirection: 'column', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        height: '100vh',  // Imposta l'altezza della sezione per centrare verticalmente
+        textAlign: 'justify', 
+        padding: '20px',
+      }}>
+        <div style={{maxWidth: '800px'}}> {/* Limita la larghezza del contenuto per una lettura migliore */}
+          <h1 style={{textAlign: 'center'}}>{title}</h1> {/* Titolo centrato */}
+          <p>{description}</p>
+          <p>
+            <strong>Abstract:</strong> This paper introduces a composite index for impunity,
+            which aims to measure regional disparities in Italy from 2008 to 2021. The index highlights
+            the significant socio-economic impacts of impunity, especially on foreign direct investments
+            and crime reporting. Through an econometric analysis, the findings emphasize the need for
+            stronger legal frameworks and targeted policies to reduce regional disparities and promote
+            socio-economic stability.
+          </p>
+        </div>
       </section>
-
     </Page>
   );
 });
