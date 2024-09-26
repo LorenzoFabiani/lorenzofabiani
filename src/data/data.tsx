1d0e6ae21d768704c4ac18b6ddc30aa1c062fe1c
LorenzoFabiani <lorenzo.fabiani1@gmail.com>
2024-09-27 00:20:50 +0200

    Update data.tsx
    Signed-off-by: LorenzoFabiani <lorenzo.fabiani1@gmail.com>

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -101,9 +101,13 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `I'm a researcher focused on the economic determinants of crime reporting, with a special interest in public policy and impunity. 
+  description: (
+     <p style={{ textAlign: 'justify' }}>
+    `I'm a researcher focused on the economic determinants of crime reporting, with a special interest in public policy and impunity. 
   My work combines economics and law to analyze how policies and economic conditions affect the likelihood of reporting crimes, particularly sexual violence. 
 Outside of research, I enjoy exploring technology and playing beach volleyball.`,
+       </p>
+    ),
   aboutItems: [
     {label: 'Location', text: 'Valencia, Spain', Icon: MapIcon},
     {label: 'Age', text: '34', Icon: CalendarIcon},
