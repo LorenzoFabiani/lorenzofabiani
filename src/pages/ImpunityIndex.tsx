f069c1f3a18f98c4f23edc2e51183a8b1ae7aa1c
LorenzoFabiani <lorenzo.fabiani1@gmail.com>
2024-09-26 18:52:38 +0200

    Update ImpunityIndex.tsx
    Signed-off-by: LorenzoFabiani <lorenzo.fabiani1@gmail.com>

--- src/pages/ImpunityIndex.tsx
+++ src/pages/ImpunityIndex.tsx
@@ -2,13 +2,6 @@ import dynamic from 'next/dynamic';
 import {FC, memo} from 'react';  // Rimosso lo spazio tra le parentesi graffe
 
 import Page from '../components/Layout/Page';
-import About from '../components/Sections/About';
-import Contact from '../components/Sections/Contact';
-import Footer from '../components/Sections/Footer';
-import Hero from '../components/Sections/Hero';
-import Portfolio from '../components/Sections/Portfolio';
-import Resume from '../components/Sections/Resume';
-import Testimonials from '../components/Sections/Testimonials';
 
 // Meta dati per la pagina Impunity Index
 const ImpunityIndexPageMeta = {
