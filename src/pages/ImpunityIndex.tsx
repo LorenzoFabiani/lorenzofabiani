d8db74594a6f40b043adbd693febb4bb30b2fc0f
LorenzoFabiani <lorenzo.fabiani1@gmail.com>
2024-09-26 18:35:43 +0200

    Update ImpunityIndex.tsx
    Signed-off-by: LorenzoFabiani <lorenzo.fabiani1@gmail.com>

--- src/pages/ImpunityIndex.tsx
+++ src/pages/ImpunityIndex.tsx
@@ -9,13 +9,12 @@ import Hero from '../components/Sections/Hero';
 import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
 import Testimonials from '../components/Sections/Testimonials';
-import {homePageMeta} from '../data/data';
 
 // eslint-disable-next-line react-memo/require-memo
 const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
 
 const Home: FC = memo(() => {
-  const {title, description} = homePageMeta;
+  const {title, description} = ImpunityIndexPageMeta;
   return (
     <Page description={description} title={title}>
       <Header />
