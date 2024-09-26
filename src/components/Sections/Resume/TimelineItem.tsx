fcc63710107365e9366dddbe28e109151c79b10d
LorenzoFabiani <lorenzo.fabiani1@gmail.com>
2024-09-27 00:46:10 +0200

    Update TimelineItem.tsx
    Signed-off-by: LorenzoFabiani <lorenzo.fabiani1@gmail.com>

--- src/components/Sections/Resume/TimelineItem.tsx
+++ src/components/Sections/Resume/TimelineItem.tsx
@@ -14,7 +14,9 @@ const TimelineItem: FC<{item: TimelineItem}> = memo(({item}) => {
           <span className="flex-1 text-sm sm:flex-none">{date}</span>
         </div>
       </div>
-      {content}
+      <p style={{textAlign: 'justify'}}>
+        {content}
+      </p>
     </div>
   );
 });
