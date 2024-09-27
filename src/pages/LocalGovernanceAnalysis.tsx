f841b152d37b3dc344fad7706f57e734b6ecd610
LorenzoFabiani <lorenzo.fabiani1@gmail.com>
2024-09-27 10:32:04 +0200

    Update LocalGovernanceAnalysis.tsx
    Signed-off-by: LorenzoFabiani <lorenzo.fabiani1@gmail.com>

--- src/pages/LocalGovernanceAnalysis.tsx
+++ src/pages/LocalGovernanceAnalysis.tsx
@@ -4,9 +4,9 @@ import {FC, memo} from 'react';
 import Page from '../components/Layout/Page';
 
 const ImpunityIndexPageMeta = {
-  title: 'The Role of Impunity: Proposal for a Composite Index',
-  description: 'Authors: Mario Eboli and Lorenzo Fabiani',
-  description2: 'Presented at the 14th Annual Conference Spanish Association of Law and Economics – AEDE, Barcelona, June 27-28, 2024.',
+  title: 'Empowering Local Governance for Creating Public Value: A Preliminary Analysis',
+  description: 'Authors: Lorenzo Fabiani and Simone Cifolelli',
+  description2: 'Presented at the 6th International Conference on Decision Economics - DECON, Salamanca, Spain (Online), June 26-28, 2024.',
 };
 
 // eslint-disable-next-line react-memo/require-memo
@@ -34,7 +34,7 @@ const ImpunityIndex: FC = memo(() => {
             <p style={{marginBottom: '20px', color: 'white'}}>{description2}</p>
           )}
           <p style={{marginBottom: '20px', color: 'white'}}>
-            <strong>Abstract:</strong> This study introduces a pioneering methodology to quantify impunity at a provincial level within Italy, focusing on the period from 2008 to 2021. By aggregating various indicators, we present the "Impunity Index" that offers insights into regional variations and trends in impunity dynamics. A novel aspect of our approach is the emphasis on economic dimensions, augmenting the traditionally criminal-centric view of impunity. Utilizing techniques like the Analytic Hierarchy Process (AHP) for variable weighting and the Compounded Average Growth Rate (CAGR) for temporal analysis, our study reveals significant regional disparities in impunity levels. Geographical analyses further underscore a north-south gradient, with northern regions typically exhibiting lower impunity levels. This study, one of the first to investigate the issue of impunity at such a granular level, offers a novel perspective on the matter and can possibly be a foundation for further research in Italy and potentially other nations.
+            <strong>Abstract:</strong> This paper investigates the impact of local governance on municipal perfor-mance, specifically through the lens of European Union (EU) fund utiliza-tion. Focusing on Italian municipalities with populations exceeding 50,000, the study explores how the characteristics of mayors - age, gender, and edu-cation -correlate with the effective use of EU resources. By analyzing a com-prehensive dataset, this preliminary analysis seeks to identify patterns and trends that clarify the role of mayoral attributes in fostering public value through efficient fund management. The findings aim to contribute to the discourse on public administration and governance, offering insights that can inform policy and support the enhancement of municipal governance structures across Italy and potentially other EU regions.
           </p>
         </div>
       </section>
